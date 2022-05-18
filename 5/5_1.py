15da4e7cbeb0840c60f7dd638a09a7a1882273f2
Rinat <rintwork@mail.ru>
2022-05-18 23:01:05 +0300

    lesson5

--- 5/5_1.py
+++
@@ -1,6 +0,0 @@
-with open('task_1.txt', 'w', encoding='utf_8') as task1:
-    while True:
-        text = input("Enter the text: ")
-        if not text:
-            break
-        task1.write(f"{text}\n")
